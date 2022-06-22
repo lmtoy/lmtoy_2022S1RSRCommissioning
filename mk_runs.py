f364d99c26f20c1a711a6e853ed64dfd63b1c005
Peter Teuben <teuben@gmail.com>
2022-06-22 12:13:18 -0400

    upd

--- mk_runs.py
+++ mk_runs.py
@@ -18,27 +18,33 @@ on['I10565'] = [94687, 94688,
 on['I12112'] = [94993, 94994, 95302, 95303,
                 98184, 98185, 98299, 98300, 98302, 98303,              
                 98422, 98423]
+
 on['I17208'] = [95306, 95307, 95524, 95525, 98594, 98595, 98649, 98650]
+
+on['mwc349a'] = [98534, 98535, 100114, 100114, 101103, 101104, 101106, 101107]
 
 
 #       common parameters per source on the first dryrun (run1, run2)
 pars1 = {}
 
-pars1['I10565'] = ""
-pars1['I12112'] = ""
-pars1['I17208'] = ""
+pars1['I10565']  = ""
+pars1['I12112']  = ""
+pars1['I17208']  = ""
+pars1['mwc349a'] = ""
 
 #        common parameters per source on subsequent runs (run1a, run2a)
 pars2 = {}
-pars2['I10565'] = "admit=0"
-pars2['I12112'] = "admit=0"
-pars2['I17208'] = "admit=0"
+pars2['I10565']  = "admit=0"
+pars2['I12112']  = "admit=0"
+pars2['I17208']  = "admit=0"
+pars2['mwc349a'] = "admit=0"
+
 
 # below here no need to change code
 # ========================================================================
 
-#        helper function for populating obsnum dependant argument
-def getargs(obsnum):
+#        helper function for populating obsnum dependant argument -- deprecated
+def getargs3(obsnum):
     """ search for <obsnum>.args
     """
     f = "%d.args" % obsnum
@@ -52,56 +58,83 @@ def getargs(obsnum):
     else:
         return ""
 
-#        specific parameters per obsnum will be in files <obsnum>.args
+#        specific parameters per obsnum will be in files <obsnum>.args -- deprecated
 pars3 = {}
 for s in on.keys():
     for o1 in on[s]:
         o = abs(o1)
-        pars3[o] = getargs(o)
+        pars3[o] = getargs3(o)
 
+#        obsnum.args is alternative single file pars file to set individual parameters
+pars4 = {}
+if os.path.exists("obsnum.args"):
+    lines = open("obsnum.args").readlines()
+    for line in lines:
+        if line[0] == '#': continue
+        w = line.split()
+        pars4[int(w[0])] = w[1:]
+        print('PJT',w[0],w[1:])
+
+def getargs(obsnum):
+    """ search for <obsnum> in obsnum.args
+    """
+    args = ""
+    if obsnum in pars4.keys():
+        print("PJT2:",obsnum,pars4[obsnum])
+        for a in pars4[obsnum]:
+            args = args + " " + a
+    return args
 
 run1  = '%s.run1'  % project
 run1a = '%s.run1a' % project
+run1b = '%s.run1b' % project
 run2  = '%s.run2' % project
 run2a = '%s.run2a' % project
 
 fp1 = open(run1,  "w")
 fp2 = open(run1a, "w")
-fp3 = open(run2,  "w")
-fp4 = open(run2a, "w")
+fp3 = open(run1b, "w")
+
+fp4 = open(run2,  "w")
+fp5 = open(run2a, "w")
 
 #                           single obsnum
 n1 = 0
 for s in on.keys():
     for o1 in on[s]:
         o = abs(o1)
-        cmd1 = "SLpipeline.sh obsnum=%d _s=%s %s admit=0 restart=1 " % (o,s,pars1[s])
-        cmd2 = "SLpipeline.sh obsnum=%d _s=%s %s admit=0 %s" % (o,s,pars2[s], pars3[o])
+        cmd1 = "SLpipeline.sh obsnum=%d _s=%s %s admit=0 restart=1 %s %s" % (o,s,pars1[s], pars2[s], getargs(o))
+        cmd2 = "SLpipeline.sh obsnum=%d _s=%s %s admit=0 restart=1" % (o,s,pars1[s])
+        cmd3 = "SLpipeline.sh obsnum=%d _s=%s %s admit=0 %s" % (o,s,pars2[s], getargs(o))
         fp1.write("%s\n" % cmd1)
         fp2.write("%s\n" % cmd2)
+        fp3.write("%s\n" % cmd3)
         n1 = n1 + 1
 
 #                           combination obsnums
 n2 = 0        
 for s in on.keys():
     obsnums = ""
+    n3 = 0
     for o1 in on[s]:
         o = abs(o1)
         if o1 < 0: continue
+        n3 = n3 + 1
         if obsnums == "":
             obsnums = "%d" % o
         else:
             obsnums = obsnums + ",%d" % o
-    print('%s[%d] :' % (s,len(on[s])), obsnums)
-    cmd3 = "SLpipeline.sh _s=%s admit=0 restart=1 obsnums=%s" % (s, obsnums)
-    cmd4 = "SLpipeline.sh _s=%s admit=1 srdp=1  obsnums=%s" % (s, obsnums)
-    fp3.write("%s\n" % cmd3)
+    print('%s[%d/%d] :' % (s,n3,len(on[s])), obsnums)
+    cmd4 = "SLpipeline.sh _s=%s admit=0 restart=1 obsnums=%s" % (s, obsnums)
+    cmd5 = "SLpipeline.sh _s=%s admit=1 srdp=1  obsnums=%s" % (s, obsnums)
     fp4.write("%s\n" % cmd4)
+    fp5.write("%s\n" % cmd5)
     n2 = n2 + 1
 
 print("A proper re-run of %s should be in the following order:" % project)
-print(run1)
-print(run2)
 print(run1a)
+print(run2)
+print(run1b)
 print(run2a)
 print("Where there are %d single obsnum runs, and %d combination obsnums" % (n1,n2))
+
